42b51504c0ef3b17c5e914ee7767795093735c55
Lokisley Oliveira <lokisley@hotmail.com>
2021-11-27 23:35:13 -0300

    feat: create header and footer components

---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <a> images by pexels.com</a>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/layout/header.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from 'react-bootstrap'
+
+function Header() {
+    return (
+        <header>
+            <Navbar className="justify-content-center" bg="light">
+                <h1> Galeria - Leadster </h1>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
