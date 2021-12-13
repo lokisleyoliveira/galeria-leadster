d99980e1b6a0022dda0b1a4da3090aff332a6d76
Lokisley Oliveira <lokisley@hotmail.com>
2021-12-12 22:25:26 -0300

    feat: create api call and adjust columns by screen width

--- src/components/layout/gallery.jsx
+++ src/components/layout/gallery.jsx
@@ -1,27 +1,42 @@
-//import { key } from '../../key.json'
+import { createClient } from 'pexels';
 import { useEffect, useState } from 'react';
 import { Button, Container } from 'react-bootstrap';
+import { key } from '../../key.json';
 
-//const client = createClient(key)
+const client = createClient(key)
+const query = "blue"
 
 function Gallery() {
 
     const [page, setPage] = useState(1)
     const [image, setImage] = useState([])
+    const [cols, setCols] = useState([3])
 
     async function loadData() {
-        console.log(page)
 
-        const h = Math.floor(Math.random() * (10 - 4 + 1) + 4) * 100
-        const w = Math.floor(Math.random() * (10 - 4 + 1) + 4) * 100
-        setImage((image) => ([...image, {
-            img:
-                `http://picsum.photos/${w}/${h}?${page}`,
-            cols: 1,
-            title: 'image',
-        }]))
+        client.photos.search({ query, per_page: 60, page: page }).then(result => {
+            result.photos.map((curr) => {
+                setImage((image) => ([...image, {
+                    img: curr.src.portrait,
+                    author: curr.photographer,
+                    link: curr.url
+                }]))
+            })
+        })
     }
 
+    async function adjustCols() {
+        const size = window.innerWidth
+
+        if (size >= 1200) setCols(5)        // lg
+        else if (size >= 992) setCols(4)    // md
+        else if (size >= 768) setCols(3)    // sm
+        else setCols(2)                     // xs
+
+        console.log(cols)
+    }
+
+    useEffect(() => adjustCols(), [window.innerWidth])
     useEffect(() => loadData(), [page])
 
     const chunkArray = (arr, size) => {
@@ -37,13 +52,16 @@ function Gallery() {
 
     return (
         <Container>
-            {console.log(chunkArray(image, 3))}
-            <div className="imgRow">
-                {chunkArray(image, 3).map((chunk) => {
+            <div className="imgRow" >
+                {chunkArray(image, cols).map((chunk) => {
                     return (
-                        <div className="imgColumn">
-                            {chunk.map(({ img }) => (
-                                <img src={img} />
+                        <div className="imgColumn" >
+                            {chunk.map(({ img, author, link }) => (
+                                <a href={link} target="_blank" className="imgDisplay">
+                                    <img src={img} />
+                                    <div className="insetShadow" />
+                                    <span> by: {author} </span>
+                                </a>
                             ))}
                         </div>
                     );
